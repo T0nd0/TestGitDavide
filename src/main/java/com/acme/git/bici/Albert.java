eaf13dda48cf46dfa5d8df0c500c677841a8b510
Albert <47365826+os3albert@users.noreply.github.com>
2022-06-09 16:22:48 +0200

    albert commit

---
+++ src/main/java/com/acme/git/bici/Albert.java
@@ -0,0 +1,14 @@
+package com.acme.git.bici;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Albert {
+	
+	private String cognome;
+
+}

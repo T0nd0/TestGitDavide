ceebd137dfbdfec73af81093fc360c0dd1ad38c0
david <david@DESKTOP-499U3MH.station>
2022-06-09 15:56:00 +0200

    first commit

---
+++ src/main/java/com/acme/git/TestGitDavideApplication.java
@@ -0,0 +1,13 @@
+package com.acme.git;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestGitDavideApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestGitDavideApplication.class, args);
+	}
+
+}
